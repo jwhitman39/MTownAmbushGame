17df1884bbd0fdc6fe3d43ed698f285fb2ce675b
jwhitman39 <jtw3909@gmail.com>
2025-05-13 12:39:01 -0700

    added animations for monster

---
+++ My project (2)/Assets/MonsterMovement.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    float horizontalInput;
+    float moveSpeed = 5f;
+    bool isFacingRight = false;
+    float jumpPower = 5f;
+    bool isGrounded = false;
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            isGrounded = true;
+            animator.SetBool("isJumping", true);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2 (horizontalInput * moveSpeed, rb.linearVelocity.y);
+        animator.SetFloat("xVelocity", Math.Abs(rb.linearVelocity.x));
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
+
+    }
+
+    void FlipSprite()
+    {
+        if(isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
+    }
+}
